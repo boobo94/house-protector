103b1946d4a63fdc4fdfcdec7fbd1732768346fc
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-10-30 20:48:21 +0200

    #11 - Change the translations

--- lambda/custom/locales/translations.ts
+++ lambda/custom/locales/translations.ts
@@ -2,8 +2,8 @@
 export const locales = {
     'en-US': {
         translation: {
-            "SKILL_NAME": "Hello world",
-            "WELCOME_MSG": "Welcome to the Alexa Skills Kit, you can say hello!",
+            "SKILL_NAME": "House Protector",
+            "WELCOME_MSG": "Welcome to House Protector.",
             "GOODBYE_MSG": "Goodbye!",
             "HELLO_MSG": "Hello world!",
             "HELP_MSG": "You can say hello to me!",
