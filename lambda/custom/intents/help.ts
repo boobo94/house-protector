ba1509c8ac111e4ddd9bf947c8a7476a1b70180d
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-10-30 21:28:05 +0200

    #12 - Implement help intent

--- lambda/custom/intents/help.ts
+++ lambda/custom/intents/help.ts
@@ -3,17 +3,17 @@ import { GetRequestAttributes, IsIntent } from '../lib/helpers';
 import { IntentTypes } from '../lib/types';
 
 export const HelpIntentHandler: RequestHandler = {
-    canHandle(handlerInput) {
-        return IsIntent(handlerInput, IntentTypes.Help)
-    },
-    handle(handlerInput) {
-        const { t } = GetRequestAttributes(handlerInput)
-        const speechText = t("HELP_MSG")
+  canHandle(handlerInput) {
+    return IsIntent(handlerInput, IntentTypes.Help)
+  },
+  handle(handlerInput) {
+    const { t } = GetRequestAttributes(handlerInput)
+    const speechText = t("HELP_MSG")
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .withSimpleCard(t('SKILL_NAME'), speechText)
-            .getResponse();
-    }
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard(t('SKILL_NAME'), speechText)
+      .getResponse();
+  }
 };
