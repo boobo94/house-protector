12473335329a86ca8b634b88fe22e69f99f8085b
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-10-30 20:48:01 +0200

    #11 - Fix the load persistence

--- lambda/custom/lib/interfaces.ts
+++ lambda/custom/lib/interfaces.ts
@@ -1,23 +1,42 @@
 import { States } from "./types";
 
 export interface RequestAttributes {
-    /**
-     * Searches for the translation of the given key
-     * @param key
-     * @param args
-     */
-    t(key: string, ...args: any[]): any;
+  /**
+   * Searches for the translation of the given key
+   * @param key
+   * @param args
+   */
+  t(key: string, ...args: any[]): any;
 
-    tr(key: string): string;
+  tr(key: string): string;
 
 }
 
 export interface SessionAttributes {
-    state: States
-    firstTimeUser: boolean;
+  state: States
+  firstTimeUser: boolean;
 
 }
 
 export interface PersistentAttributes {
+  /**
+   * The version of the attributes. If the version doesn't match with the `VERSION` in `lib/constants.ts`,
+   * the attributes will be reset.
+   */
+  version: string;
 
+  /**
+    * Playback details.
+    */
+  playbackInfo: {
+
+    /**
+     * The token of the current audio.
+     */
+    token: string;
+
+    offsetInMilliseconds: number;
+
+    nextStreamEnqueued: boolean;
+  };
 }
