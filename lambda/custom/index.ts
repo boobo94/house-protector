b8a574e418d0050251b0d506087de1ac7c12ea55
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-10-30 21:18:31 +0200

    #13 - Implement stop and cancel intents

--- lambda/custom/index.ts
+++ lambda/custom/index.ts
@@ -5,30 +5,31 @@ import * as Interceptors from './interceptors'
 import { config } from './interceptors/config';
 
 export const handler = SkillBuilders.custom()
-    .addRequestHandlers(
+  .addRequestHandlers(
 
-        // built in intents
-        Intents.LaunchRequestHandler,
-        Intents.HelpIntentHandler,
-        Intents.CancelAndStopIntentHandler,
-        Intents.SessionEnded,
-        Intents.Fallback,
+    // built in intents
+    Intents.LaunchRequestHandler,
+    Intents.HelpIntentHandler,
+    Intents.CancelIntentHandler,
+    Intents.StopIntentHandler,
+    Intents.SessionEnded,
+    Intents.Fallback,
 
-        // custom
-        Intents.ProtectIntentHandler,
+    // custom
+    Intents.ProtectIntentHandler,
 
-    )
-    .addErrorHandlers(
-        Errors.Unknown,
-    )
-    .addRequestInterceptors(
-        Interceptors.LoadPersistentAttributes,
-        Interceptors.Localization,
-    ).addResponseInterceptors(
-        Interceptors.SavePersistentAttributes,
-    )
-    .withPersistenceAdapter(new DynamoDbPersistenceAdapter({
-        createTable: true,
-        tableName: config.dynamoDbTableName,
-    }))
-    .lambda();
+  )
+  .addErrorHandlers(
+    Errors.Unknown,
+  )
+  .addRequestInterceptors(
+    Interceptors.LoadPersistentAttributes,
+    Interceptors.Localization,
+  ).addResponseInterceptors(
+    Interceptors.SavePersistentAttributes,
+  )
+  .withPersistenceAdapter(new DynamoDbPersistenceAdapter({
+    createTable: true,
+    tableName: config.dynamoDbTableName,
+  }))
+  .lambda();
