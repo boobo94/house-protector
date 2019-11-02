676bea5ff4700b409a89e3fadeba457ff4850dbe
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-11-02 16:18:17 +0200

    fix: Cancel the skill on stop intent

--- lambda/custom/intents/stop.ts
+++ lambda/custom/intents/stop.ts
@@ -1,12 +1,13 @@
 import { RequestHandler } from "ask-sdk";
 import { IsIntent } from '../lib/helpers';
 import { IntentTypes } from "../lib/types";
+import { CancelIntentHandler } from "./cancel";
 
 export const StopIntentHandler: RequestHandler = {
   canHandle(handlerInput) {
     return IsIntent(handlerInput, IntentTypes.Stop);
   },
   handle(handlerInput) {
-    return StopIntentHandler.handle(handlerInput);
+    return CancelIntentHandler.handle(handlerInput);
   }
 };
