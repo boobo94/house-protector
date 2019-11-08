c23a02f3acce61d5bc9f2ce225831bc7c968c5a7
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-11-08 23:07:43 +0200

    #1 - Add pause sounds

--- lambda/custom/lib/sounds-manager/sounds.ts
+++ lambda/custom/lib/sounds-manager/sounds.ts
@@ -1,12 +1,24 @@
 import { AudioType } from "../interfaces";
 import { SoundTokens } from "./interfaces";
 
-const pauses: AudioType[] = [
+const getPauses = (token): AudioType[] => [
   {
-    token: `${SoundTokens.BARKS}_silent_initial`,
-    url: ''
-  }
-];
+    token: `${token}_pause1`,
+    url: 'https://house-protector-alexa-skill.s3-eu-west-1.amazonaws.com/pause-45s.mp3'
+  },
+  {
+    token: `${token}_pause2`,
+    url: 'https://house-protector-alexa-skill.s3-eu-west-1.amazonaws.com/pause-1m.mp3'
+  },
+  {
+    token: `${token}_pause3`,
+    url: 'https://house-protector-alexa-skill.s3-eu-west-1.amazonaws.com/pause-2m30s.mp3'
+  },
+  {
+    token: `${token}_pause4`,
+    url: 'https://house-protector-alexa-skill.s3-eu-west-1.amazonaws.com/pause-5m.mp3'
+  },
+]
 
 export const sounds = {
   initial: [
@@ -107,8 +119,8 @@ export const sounds = {
   profiles: {
     default: [
       {
-        token: `${SoundTokens.BARKS}_labrador_43`,
-        url: ''
+        token: `${SoundTokens.BARKS}_default_1`,
+        url: 'https://house-protector-alexa-skill.s3-eu-west-1.amazonaws.com/profile-tv1.mp3'
       }
     ] as AudioType[],
 
@@ -128,8 +140,8 @@ export const sounds = {
 
     living: [
       {
-        token: `${SoundTokens.BARKS}_labrador_47`,
-        url: ''
+        token: `${SoundTokens.BARKS}_living_1`,
+        url: 'https://house-protector-alexa-skill.s3-eu-west-1.amazonaws.com/profile-tv1.mp3'
       }
     ] as AudioType[],
   },
@@ -137,12 +149,12 @@ export const sounds = {
     initial: [
       {
         token: `${SoundTokens.BARKS}_silent_initial`,
-        url: ''
+        url: 'https://house-protector-alexa-skill.s3-eu-west-1.amazonaws.com/pause-5s.mp3'
       }
     ] as AudioType[],
 
-    generalBarks: pauses,
-    generalProfiles: pauses,
+    generalBarks: getPauses(SoundTokens.PAUSE_GENERAL_BARKS),
+    generalProfiles: getPauses(SoundTokens.PAUSE_GENERAL_PROFILES),
 
   }
 }
