67f20c803ab4df876b0c2f10e91a86803df9f687
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-11-09 12:21:10 +0200

    Rename the invocation name in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,92 +9,92 @@ const IN_DIR = "lambda/custom/";
 const OUT_DIR = "dist/custom/";
 
 gulp.task("tsc", function (done) {
-    const tscPath = path.normalize("./node_modules/.bin/tsc");
-    const command = `${tscPath} -p tsconfig.json`;
+  const tscPath = path.normalize("./node_modules/.bin/tsc");
+  const command = `${tscPath} -p tsconfig.json`;
 
-    child_process.execSync(command, {
-        stdio: "inherit"
-    });
-    done();
+  child_process.execSync(command, {
+    stdio: "inherit"
+  });
+  done();
 });
 
 // gulp.task("test", gulp_base.test);
 
 gulp.task("json", function () {
-    return gulp.src(IN_DIR + '/**/package.json').pipe(gulp.dest(OUT_DIR));
+  return gulp.src(IN_DIR + '/**/package.json').pipe(gulp.dest(OUT_DIR));
 });
 
 gulp.task("models", function (done) {
-    const fs = require("fs");
+  const fs = require("fs");
 
-    /**
-     * Reads the model for the given locale and returns the parsed JSON.
-     *
-     * @param {string} locale
-     */
-    function readModel(locale) {
-        const model = fs.readFileSync(`${__dirname}/models/${locale}.json`, "utf-8");
-        return JSON.parse(model);
-    }
+  /**
+   * Reads the model for the given locale and returns the parsed JSON.
+   *
+   * @param {string} locale
+   */
+  function readModel(locale) {
+    const model = fs.readFileSync(`${__dirname}/models/${locale}.json`, "utf-8");
+    return JSON.parse(model);
+  }
 
-    /**
-     * Writes the given model to the file.
-     *
-     * @param {object} model
-     * @param {string} locale
-     */
-    function writeModel(model, locale) {
-        const json = JSON.stringify(model, null, 2);
-        fs.writeFileSync(`${__dirname}/models/${locale}.json`, json);
-    }
+  /**
+   * Writes the given model to the file.
+   *
+   * @param {object} model
+   * @param {string} locale
+   */
+  function writeModel(model, locale) {
+    const json = JSON.stringify(model, null, 2);
+    fs.writeFileSync(`${__dirname}/models/${locale}.json`, json);
+  }
 
-    const Locales = {
-        enUS: "en-US"
-    };
+  const Locales = {
+    enUS: "en-US"
+  };
 
-    const Environments = {
-        Dev: "dev",
-        Prod: "prod",
-    };
+  const Environments = {
+    Dev: "dev",
+    Prod: "prod",
+  };
 
-    const invocations = {
-        [Environments.Dev]: {
-            [Locales.enUS]: "greeter",
-        },
-        [Environments.Prod]: {
-            [Locales.enUS]: "greeter",
-        },
-    };
+  const invocations = {
+    [Environments.Dev]: {
+      [Locales.enUS]: "house protector",
+    },
+    [Environments.Prod]: {
+      [Locales.enUS]: "house protector",
+    },
+  };
 
-    // make sure we have the environment set
-    if (!process.env.ENV) {
-        throw new Error("ENV environment variable not set");
-    }
+  // make sure we have the environment set
+  if (!process.env.ENV) {
+    throw new Error("ENV environment variable not set");
+  }
 
-    // get the current environment
-    const env = process.env.ENV;
+  // get the current environment
+  const env = process.env.ENV;
 
-    // make sure the env is valid
-    if (env !== Environments.Local
-        && env !== Environments.Dev
-        && env !== Environments.Prod) {
-        throw new Error("Invalid ENV environment variable: " + env);
-    }
+  // make sure the env is valid
+  if (env !== Environments.Local
+    && env !== Environments.Dev
+    && env !== Environments.Prod) {
+    throw new Error("Invalid ENV environment variable: " + env);
+  }
 
-    // read the models
-    const enUSModel = readModel(Locales.enUS);
+  // read the models
+  const enUSModel = readModel(Locales.enUS);
 
-    // set the invocation names
-    enUSModel.interactionModel.languageModel.invocationName = invocations[env][Locales.enUS];
+  // set the invocation names
+  enUSModel.interactionModel.languageModel.invocationName = invocations[env][Locales.enUS];
 
-    // write the model to the file
-    writeModel(enUSModel, Locales.enUS);
+  // write the model to the file
+  writeModel(enUSModel, Locales.enUS);
 
-    done();
+  done();
 });
 
 gulp.task("clean", () => {
-    return del([".coverage", "dist"]);
+  return del([".coverage", "dist"]);
 });
 
 gulp.task("default", gulp.series("clean", gulp.parallel("tsc", "json")));
