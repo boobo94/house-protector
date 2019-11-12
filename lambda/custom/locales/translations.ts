cf5bc5bf9bfc3d4fb278aa8eed8ea652a4e79e25
boobo94 <militaru.bogdanalexandru@gmail.com>
2019-11-12 20:32:00 +0200

    (#22) Update translations

--- lambda/custom/locales/translations.ts
+++ lambda/custom/locales/translations.ts
@@ -18,12 +18,12 @@ export const locales = {
       [TranslationsKeys.WELCOME]: "Welcome to House Protector.",
       [TranslationsKeys.WELCOME_SHORT]: "Welcome to House Protector. I can help you to change your favorite dog, room profile or protect your home. What can I do for you?",
       [TranslationsKeys.GOODBYE]: "Goodbye!",
-      [TranslationsKeys.HELP]: "House protector is a skill which aims to protect your home when you leave it. When you're leaving I'll play sounds for you. The room profiles available are: LIVING ROOM, KITCHEN, BEDROOM. Change them by simply ask to change the profile. The dog profiles availables are: LABRADOR, DOBERMAN, GERMAN SHEPARD. You can change the dog by asking to change the dog. What do want next?",
+      [TranslationsKeys.HELP]: "House protector is a skill that aims to protect your home when you leave. I'll simulate a real environment reproducing real human existence. The room profiles available are LIVING ROOM, KITCHEN, BEDROOM. Change the profile according to your preferences. The dog profile available are LABRADOR, DOBERMAN, GERMAN SHEPARD, SMALL. Change the profile according to your preferences. What do you want now?",
       [TranslationsKeys.GENERAL_ERROR]: "Sorry, I can't understand the command. Please say it again.",
-      [TranslationsKeys.UNEXPECTED_ERROR]: "Sorry, an unexpected error has occured. Please try again later.",
+      [TranslationsKeys.UNEXPECTED_ERROR]: "Sorry, an unexpected error has occurred. Please try again later.",
 
-      [TranslationsKeys.CONFIG_DOG_CONFIRMATION]: "The current dog selected is %s. The options available are: labrador, doberman, small dog and german shepard. What is your option?",
-      [TranslationsKeys.CONFIG_PROFILE_CONFIRMATION]: "The current profile selected is %s. The options available are: default, kitchen, bedroom and living room. What is your option?"
+      [TranslationsKeys.CONFIG_DOG_CONFIRMATION]: "The active dog is %s. The options available are labrador, Doberman, small dog and german Shepard. You can start the guard mode or change the dog by your preference. What is your option?",
+      [TranslationsKeys.CONFIG_PROFILE_CONFIRMATION]: "The active profile is %s. The options available are default, kitchen, bedroom and living room. You can start the guard mode or change the profile by your preference. What is your option?",
     }
   },
 };
